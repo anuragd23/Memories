04f4cda92ca6d6fbf2fbe8fef1b03bad4b658dbf
Anurag Das <anurag.das23.ad@gmail.com>
2021-09-11 09:46:21 -0400

    Server setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose  from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
